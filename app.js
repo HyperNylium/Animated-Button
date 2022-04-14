fac68d7585d1d346028f1b8358b09d09233ebb75
HyperNylium <davidpivik123@gmail.com>
2022-04-14 22:16:33 +0300

    main

---
+++ app.js
@@ -0,0 +1,43 @@
+const BTN = document.querySelector("[data-BTN]")
+
+BTN.addEventListener("click", () => {
+    setTimeout(() => {
+        window.location.href='#'
+    }, 500)
+
+    setTimeout(() => {
+        window.location.href='##'
+    }, 1000)
+    
+    setTimeout(() => {
+        window.location.href='###'
+    }, 1500)
+
+    setTimeout(() => {
+        window.location.href='####'
+    }, 2000)
+
+    setTimeout(() => {
+        window.location.href='#####'
+    }, 2500)
+
+    setTimeout(() => {
+        window.location.href='######'
+    }, 3000)
+
+    setTimeout(() => {
+        window.location.href='#######'
+    }, 3500)
+    
+    setTimeout(() => {
+        window.location.href='########'
+    }, 4000)
+
+    setTimeout(() => {
+        window.location.href='#########'
+    }, 4500)
+
+    setTimeout(() => {
+        window.location.href='##########'
+    }, 5000)
+})
